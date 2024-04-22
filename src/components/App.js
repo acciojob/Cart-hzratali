10e53518e82faba84a696f6fa559dd5d2abc4042
Hazrat Ali <hazrat8143@gmail.com>
2024-04-23 01:52:06 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
-import React from "react";
-import Navbar from "./Navbar";
-import CartContainer from "./CartContainer";
-import { useGlobalContext } from "./context";
+import React, { useContext } from 'react'
+import cartContext from '../context/cartContext'
+import Navbar from './Navbar'
 
 const App = () => {
-  const { loading } = useGlobalContext();
+    const { cart, dispatch } = useContext(cartContext)
 
-  if (loading) {
     return (
-      <div className="loading">
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-  return (
-    <main id="main">
-      <Navbar />
-      <CartContainer />
-    </main>
-  );
-};
+        <div id='main'>
+            <Navbar />
+            <button id='clear-all-cart' onClick={() => dispatch({ type: "clearcart" })} >Clear Cart</button>
+            <div>
+                {
+                    cart.length == 0 ?
+                        <p>Cart is currently empty</p>
+                        :
+                        <div id="cart-items-list">
+                            {cart.map(item => {
+                                return (
+                                    <div key={item.id} style={{ border: "1px solid black" }} >
+                                        <h3>{item.name}</h3>
+                                        <p id={"cart-item-price-" + item.id} >${item.price}</p>
+                                        <button id={`increment-btn-${item.id}`} onClick={() => dispatch({ type: "increment", id: item.id })}>+</button>
+                                        <p id={"cart-amount-" + item.id} >{item.qty}</p>
+                                        <button id={`decrement-btn-${item.id}`} onClick={() => dispatch({ type: "decrement", id: item.id })}>-</button>
+                                        <button id={"cart-item-remove-" + item.id} onClick={() => dispatch({ type: "remove", id: item.id })} >Remove</button>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                }
+            </div>
+        </div>
+    )
+}
 
-export default App;
+export default App
